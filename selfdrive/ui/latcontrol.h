5c1bf872e63ac286c00e20d152b35e3929a7473a
Vehicle Researcher <>
2021-02-28 03:04:41 +0900

    openpilot v0.8.2

---
+++ selfdrive/ui/latcontrol.h
@@ -0,0 +1,125 @@
+#include <stdlib.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "common/params.h"
+#include "ui.hpp"
+
+
+bool control_button_clicked1(int touch_x, int touch_y) {
+  if (touch_x >= 1585 && touch_x <= 1725) {
+    if (touch_y >= 905 && touch_y <= 1045) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool control_button_clicked2(int touch_x, int touch_y) {
+  if (touch_x >= 1425 && touch_x <= 1565) {
+    if (touch_y >= 905 && touch_y <= 1045) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool control_button_screenshot(int touch_x, int touch_y) {
+  if (touch_x >= 0 && touch_x <= 150) {
+    if (touch_y >= 465 && touch_y <= 615) {
+      return true;
+    }
+  }
+  return false;
+}
+
+static void draw_control_button1(UIState *s, int touch_x, int touch_y) {
+  if (s->vipc_client->connected || s->is_OpenpilotViewEnabled) {
+    int btn_w = 140;
+    int btn_h = 140;
+    int btn_x1 = 1920 - btn_w - 195;
+    int btn_y = 1080 - btn_h - 35;
+    int btn_xc1 = btn_x1 + (btn_w/2);
+    int btn_yc = btn_y + (btn_h/2);
+    nvgBeginPath(s->vg);
+    nvgRoundedRect(s->vg, btn_x1, btn_y, btn_w, btn_h, 100);
+    nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
+    nvgStrokeWidth(s->vg, 6);
+    nvgStroke(s->vg);
+    
+    nvgFontSize(s->vg, 45);
+    
+    if (s->lat_mode == 0) {
+      nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
+      nvgText(s->vg,btn_xc1,btn_yc,"NONE",NULL);
+    } else if (s->lat_mode == 1) {
+      nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
+      nvgText(s->vg,btn_xc1,btn_yc,"CURV",NULL);
+    } else if (s->lat_mode == 2) {
+      nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
+      nvgText(s->vg,btn_xc1,btn_yc,"ONEW",NULL);
+    }
+  }
+}
+
+static void draw_control_button2(UIState *s, int touch_x, int touch_y) {
+  if (s->vipc_client->connected || s->is_OpenpilotViewEnabled) {
+    int btn_w = 140;
+    int btn_h = 140;
+    int btn_x2 = 1920 - btn_w - 355;
+    int btn_y = 1080 - btn_h - 35;
+    int btn_xc2 = btn_x2 + (btn_w/2);
+    int btn_yc = btn_y + (btn_h/2);
+    nvgBeginPath(s->vg);
+    nvgRoundedRect(s->vg, btn_x2, btn_y, btn_w, btn_h, 100);
+    nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
+    nvgStrokeWidth(s->vg, 6);
+    nvgStroke(s->vg);
+    
+    nvgFontSize(s->vg, 45);
+    nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
+    nvgText(s->vg,btn_xc2,btn_yc,"NAVI",NULL);
+  }
+}
+
+bool latcontrol( UIState *s, int touch_x, int touch_y ) {
+
+  bool touched = false;
+
+  draw_control_button1(s, touch_x, touch_y);
+  draw_control_button2(s, touch_x, touch_y);
+
+  if ((control_button_clicked1(touch_x,touch_y)) && (s->status != STATUS_OFFROAD) && (s->limit_set_speed == 0) && (s->sidebar_collapsed == true)) {
+    s->lat_mode = s->lat_mode + 1;
+    if (s->lat_mode > 2) {
+      s->lat_mode = 0;
+    }
+    if (s->lat_mode == 0) {
+      Params().write_db_value("OpkrLatMode", "0", 1);
+    } else if (s->lat_mode == 1) {
+      Params().write_db_value("OpkrLatMode", "1", 1);
+    } else if (s->lat_mode == 2) {
+      Params().write_db_value("OpkrLatMode", "2", 1);
+    }
+    touched = true;
+  }
+
+  if ((control_button_clicked2(touch_x,touch_y)) && (s->status != STATUS_OFFROAD) && (s->limit_set_speed == 0) && (s->sidebar_collapsed == true)) {
+    Params().write_db_value("LimitSetSpeed", "1", 1);
+    //system("su -c 'am start -n com.gmd.hidesoftkeys/com.gmd.hidesoftkeys.MainActivity'");
+    system("su -c 'am start --activity-task-on-home com.skt.tmap.ku/com.skt.tmap.activity.TmapNaviActivity'");
+    s->sound->play(AudibleAlert::CHIME_WARNING1);
+    touched = true;
+  }
+
+  if ((control_button_screenshot(touch_x,touch_y)) && (s->status != STATUS_OFFROAD)) {
+    Params().write_db_value("LimitSetSpeed", "0", 1);
+    system("su -c 'am start --activity-task-on-home ai.comma.plus.offroad/.MainActivity'");
+    s->sound->play(AudibleAlert::CHIME_WARNING1);
+    touched = true;
+  }
+
+  return touched;
+}
